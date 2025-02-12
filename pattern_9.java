2130c77ada82dee2f5ac7f1cfbb6dc12a1a2203f
Yash Malvade <yashmalvade91@gmail.com>
2025-02-12 20:16:41 +0530

    Create pattern_9.java

---
+++ pattern_9.java
@@ -0,0 +1,28 @@
+
+// Pattern 9
+import java.util.*;
+
+public class patterns {
+    public static void main(String[] args) {
+        int n = 5;
+        // outer loop
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                int sum = i + j;
+                if (sum % 2 == 0) { // even
+                    System.out.print("1 ");
+                } else { // odd
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+*/  
+1 
+0 1
+1 0 1
+0 1 0 1
+1 0 1 0 1 
+/*
