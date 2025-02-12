d2855a26ef7ee4716fa86373d59f5052a34fbe86
Yash Malvade <yashmalvade91@gmail.com>
2025-02-12 16:18:21 +0530

    Create pattern_4.java

---
+++ pattern_4.java
@@ -0,0 +1,29 @@
+
+// Pattern 4
+import java.util.*;
+
+public class patterns {
+    public static void main(String[] args) {
+        int n = 4;
+
+        // outer loop
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // inner loop
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+   *
+  **
+ ***
+****
+
+ */
