73eb1126a6b974a57a9dd8ca5b99deee74579045
Yash Malvade <yashmalvade91@gmail.com>
2025-02-12 19:59:05 +0530

    Create pattern_8.java

---
+++ pattern_8.java
@@ -0,0 +1,28 @@
+
+// Pattern 8  //Floyd's Triangle
+import java.util.*;
+
+public class patterns {
+    public static void main(String[] args) {
+        int n = 5;
+        int number = 1;
+
+        // outer loop
+        for (int i = 1; i <= n; i++) {
+
+            // inner loop
+            for (int j = 1; j <= i; j++) {
+                System.out.print(number + " ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+1 
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+*/
