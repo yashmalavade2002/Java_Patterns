f7ce4d6e1a94ac30dc2fd33aa32c2913119981e3
Yash Malvade <yashmalvade91@gmail.com>
2025-02-12 16:05:28 +0530

    Create pattern_3.java

---
+++ pattern_3.java
@@ -0,0 +1,27 @@
+
+// Pattern 3
+import java.util.*;
+
+public class patterns {
+    public static void main(String[] args) {
+        int n = 5;
+
+        // outer loop
+        for (int i = n; i >= 1; i--) {
+            // inner loop
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+
+*****
+****
+***
+**
+*
+
+ */
