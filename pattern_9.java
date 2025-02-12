51cf897a815cd48fc5155760af421377f56f783a
Yash Malvade <yashmalvade91@gmail.com>
2025-02-12 20:17:05 +0530

    Update pattern_9.java

--- pattern_9.java
+++ pattern_9.java
@@ -7,6 +7,7 @@ public class patterns {
         int n = 5;
         // outer loop
         for (int i = 1; i <= n; i++) {
+            // innner loop
             for (int j = 1; j <= i; j++) {
                 int sum = i + j;
                 if (sum % 2 == 0) { // even
