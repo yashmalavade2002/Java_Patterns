38e9b3f71b7d7b45fb1de9622c857d1305ecc600
Yash Malvade <yashmalvade91@gmail.com>
2025-02-14 13:16:46 +0530

    Create pattern_15.java

---
+++ pattern_15.java
@@ -0,0 +1,21 @@
+// pattern 15
+public class patterns {
+
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+ * 1
+ * 2 1
+ * 3 2 1
+ * 4 3 2 1
+ * 5 4 3 2 1
+ */
