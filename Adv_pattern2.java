a884726025e57695938b1819dfc981c786a43822
Yash Malvade <yashmalvade91@gmail.com>
2025-02-26 14:08:56 +0530

    Create Adv_pattern2.java

---
+++ Adv_pattern2.java
@@ -0,0 +1,25 @@
+public class pattern {
+
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
+/*
+    *****
+   *****
+  *****
+ *****
+*****
+ */
