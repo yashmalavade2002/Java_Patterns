4396719015e8e8708b3d514e409996bdb9a6e3c1
Yash Malvade <yashmalvade91@gmail.com>
2025-02-26 13:55:18 +0530

    Create Adv_pattern1.java

---
+++ Adv_pattern1.java
@@ -0,0 +1,50 @@
+// Butterfly Pattern
+public class pattern {
+
+    public static void main(String[] args) {
+        int n = 4;
+        // upper half
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // spaces
+            int spaces = 2 * (n - i);
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+            // 2nd part
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // lower half
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // spaces
+            int spaces = 2 * (n - i);
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+            // 2nd part
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
+    /*
+*      *
+**    **
+***  ***
+********
+********
+***  ***
+**    **
+*      *
+   */
